e0ee3ab8375402444f5b508fc79bf788e237f4af
Joel Turbi <ignis17@pop-os.localdomain>
2019-02-17 15:36:45 -0500

    new changes

--- Lab Assignments/Lab Assignment 2/Games.java
+++ Lab Assignments/Lab Assignment 2/Games.java
@@ -1,6 +1,6 @@
 /*
   Name: Joel Turbi
-  Assignment: Games Library
+  Assignment: Lab Assignment 2 - Games Library.
   Course: CS 355
   Professor: Dr. Zavala
   Date: 02/11/2019
@@ -14,19 +14,17 @@ import java.io.*;
 // Games library Definition:
 class Games{
   public static String poem(String name){
-    return("My student " + name + " standing proud, is a fine example for the crowd.");
+    return("\nMy student " + name + " standing proud, is a fine example for the crowd.");
   }
 
   public static String fortuneTeller(String name)
   {
     String [] fortune = {"Skills comes from diligence.","Pleasures awaits you by the sea.","Beauty surrounds you because you create it.","The star of riches is shinning on you.","You are one smart cookie!"};
-    int number = (int) (Math.random() * fortune.length);
-    return("Here is your fortune " + name + ":\n" + fortune[number]);
+    int number = (int) (Math.random() * fortune.length+1);
+    return("\nHere is your fortune " + name + ":\n" + fortune[number]);
   }
 
-  /public static String madLibs(String [] inputs){
-    return("Sleep-" + inputs[0] + "is a/an " + inputs[1] " phenomenon that a surprising number of " + inputs[2] + "experience. Usually, sleepwalkers climb out of their " + inputs[3] + " and begin to " + inputs[4] + " with their " + inputs[5] + " tightly shut. Sometimes they " + inputs[6] + " outdoors wearing only their " + inputs[7] + " pajamas.");
-    // Shorten the mabLibs.
+  public static String madLibs(String [] inputs){
+   return("\nSleep-" + inputs[0] + " is a/an " + inputs[1] + " phenomenon that a \nsurprising number of " + inputs[2] + " experience. Usually, sleepwalkers climb\nout of their " + inputs[3] + " and begin to " + inputs[4] + " with their\n" + inputs[5] + " tightly shut. Sometimes they " + inputs[6] + "\noutdoors wearing only their " + inputs[7] + " pajamas." + " And it's not\nuncommon for " + inputs[8] + "-walkers to raid the " + inputs[9] + " and eat lots\nof " + inputs[10] + ". What's truly amazing is that they don't remember a/an\n" + inputs[11] + " thing the following " + inputs[12] + ". They\'ll open the fridge\nand say, \"" + inputs[13] + "! Where did all the " + inputs[14] + " go?\" They may\nnever know!");
   }
-
 }
--- Lab Assignments/Lab Assignment 2/Main.java
+++ Lab Assignments/Lab Assignment 2/Main.java
@@ -1,3 +1,10 @@
+/*
+  Name: Joel Turbi
+  Assignment: Lab Assignment 2 - Games Library.
+  Course: CS 355
+  Professor: Dr. Zavala
+  Date: 02/11/2019
+*/
 
 // Definitions of library
 import java.util.*;
@@ -7,7 +14,7 @@ import java.io.*;
 
 // Main class Definition:
 class Main{
-public static void main(String[] args) {
+public static void main(String [] args) {
 System.out.println("WELCOME");
 System.out.println("1 - Poem");
 System.out.println("2 - Fortune Teller");
@@ -18,29 +25,28 @@ Scanner in = new Scanner(System.in);
 int choice = in.nextInt();
 switch(choice){
   case 1:
-    System.out.println("Enter your name:");
+    System.out.println("\nEnter your name:");
     String name1 = in.next();
     System.out.println(Games.poem(name1));
     break;
   case 2:
-    System.out.println("Enter your name:");
+    System.out.println("\nEnter your name:");
     String name2 = in.next();
     String fortuneOut = Games.fortuneTeller(name2);
     System.out.println(fortuneOut);
     break;
   case 3:
-    String [] inputs= new String [8];
-    for(int i=0; i < 8; i++){
-      System.out.println("Enter a your input:");
+    String [] inputs= new String [15];
+    String [] choices = {"verb, ending in \"ING\"", "adjective", "plural noun", "plural noun", "verb", "part of a body (plural)", "verb", "adjective", "noun", "noun", "plural noun", "adjective", "noun", "silly word", "plural noun"};
+    for(int i=0; i < 15; i++){
+      System.out.println("Enter a " + choices[i] + ":");
       String input = in.next();
       inputs[i] = input;
-      System.out.println(Games.madLibs(inputs[]));
     }
+    System.out.println(Games.madLibs(inputs));
     break;
   default:
-    System.out.print("Invalid choice, try again.");
+    System.out.print("\nInvalid choice, try again.");
   }
 }
-
-
 }
